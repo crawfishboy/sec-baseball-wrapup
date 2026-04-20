01930812b766975d1a26fcc3884be4fe022c252b
crawfishboy <74839338+crawfishboy@users.noreply.github.com>
2026-04-20 19:23:33 -0400

    Delete assets/js/test-layout.js

--- assets/js/test-layout.js
+++
@@ -1,8 +0,0 @@
-async function loadPartial(id, file) {
-  const res = await fetch(file);
-  const html = await res.text();
-  document.getElementById(id).innerHTML = html;
-}
-
-loadPartial("header", "partials/header.html");
-loadPartial("footer", "partials/footer.html");
